50edf3d8a082696ada02b94c0b2ea4272e0eaed7
webAqemu <notych2812@gmail.com>
2020-12-27 23:51:06 +0300

    fixed animation and .btn

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ timeLine
   .from('.main-img img', { scale: 1.5 }, '-=.5')
   .from('.main-btn', { opacity: 0 })
   .from('nav', { y: '-100%' }, '-=.3')
+  .from('.about', { opacity: 0 })
   .from('.squares', { opacity: 0 })
 
 // const mouse = document.querySelector('.cursor');
