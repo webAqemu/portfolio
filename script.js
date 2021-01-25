546dc09cf32992722df38dfa33c12948c9356c4c
webAqemu <notych2812@gmail.com>
2021-01-25 14:27:13 +0300

    upgrade js animation

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-if (window.innerWidth > 760) {
+function init() {
+  if (window.innerWidth > 760) {
 
-  const mainImg = document.querySelector('.main-img');
-  let imgTop;
+    const mainImg = document.querySelector('.main-img');
+    let imgTop;
 
-  imgTop = mainImg.getBoundingClientRect().top;
-  mainImg.style.top = imgTop + 'px';
+    imgTop = mainImg.getBoundingClientRect().top;
+    mainImg.style.top = imgTop + 'px';
 
-} else {
-  let body = document.querySelector('body')
-  body.style.cssText = 'overflow-x: hidden'
+  } else {
+    let body = document.querySelector('body')
+    body.style.cssText = 'overflow-x: hidden'
+  };
+
+  const timeLine = gsap.timeline({
+    duration: 0.5,
+  });
+
+  timeLine
+    .to('.bar', { x: '100%' })
+    .to('.loading', { y: '-100%' })
+    .to('.main-box', { x: '100%' })
+    .from('.main-img img', { scale: 1.5 }, '-=.5')
+    .from('.main-btn', { opacity: 0 })
+    .from('nav', { y: '-100%' }, '-=.3')
+    .from('.about', { opacity: 0 })
+    .from('.squares', { opacity: 0 })
 };
 
-const timeLine = gsap.timeline({
-  duration: 0.5,
+window.addEventListener('load', function () {
+  init();
 });
 
-timeLine
-  .from('.bar', { x: '-100%' })
-  .to('.loading', { y: '-100%' })
-  .to('.main-box', { x: '100%' })
-  .from('.main-img img', { scale: 1.5 }, '-=.5')
-  .from('.main-btn', { opacity: 0 })
-  .from('nav', { y: '-100%' }, '-=.3')
-  .from('.about', { opacity: 0 })
-  .from('.squares', { opacity: 0 })
 
 // const mouse = document.querySelector('.cursor');
 // // const mouseTxt = mouse.querySelector('span');
